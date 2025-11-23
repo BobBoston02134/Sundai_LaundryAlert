978fa5d5376e99a8289bf63ab5d380e4e8aa0729
Will Sarg <12886992+willsarg@users.noreply.github.com>
2025-11-23 14:31:04 -0500

    s3 bucket configured

--- terraform_code/s3.tf
+++ terraform_code/s3.tf
@@ -5,10 +5,35 @@ resource "aws_s3_bucket" "laundry_alert" {
 resource "aws_s3_bucket_public_access_block" "laundry_alert" {
   bucket = aws_s3_bucket.laundry_alert.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "laundry_alert" {
+  bucket     = aws_s3_bucket.laundry_alert.id
+  depends_on = [aws_s3_bucket_public_access_block.laundry_alert]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.laundry_alert.arn}/*"
+      },
+      {
+        Sid       = "PublicPutObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.laundry_alert.arn}/*"
+      }
+    ]
+  })
 }
 
 resource "aws_s3_bucket_versioning" "laundry_alert" {
